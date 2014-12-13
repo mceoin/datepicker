5d1c8e3eae841b59faa6a14cf02668d623557f52
Eoin McMillan <eoinmcmillan@gmail.com>
2014-12-13 05:40:15 -0800

    knockout removed (date objects all fooey), date objects updating correctly via jquery

--- app.js
+++ app.js
@@ -7,11 +7,83 @@ todayDate = todayObject.getDate();
 todayDay = todayObject.getDay(); // n.b. returns array position of [S,M,T,W,T,F,S]
 daysThisMonth = daysInMonth(todayMonth, todayYear)
 
-function daysInMonth(month,year) { //Month is 1 based
-    return new Date(year, month, 0).getDate();
+currentDateObject = todayObject
+currentYear = todayYear
+currentMonth = todayMonth
+currentDay = todayDate
+
+// var d = new Date(year, month, day);
+
+// update date functions BEGIN //
+updateDateObject = function(){
+  currentDateObject = new Date(currentYear, currentMonth, currentDay);
+   console.log("-----------------------------------------------");
+  console.log("The current date is: " + currentDateObject);
+  console.log("-----------------------------------------------");
+  currentYear = currentDateObject.getFullYear()
+  currentMonth = currentDateObject.getMonth()
+  currentDay = currentDateObject.getDate()
+
+  console.log("The current year is: " + currentYear);
+  console.log("The current month is: " + currentMonth);
+  console.log("The current day is: " + currentDay);
 }
 
 
+upYear = function(){
+  currentYear = currentYear + 1;
+  updateDateObject();
+  $('.yearValue').text(currentYear)
+}
+
+downYear = function(){
+  currentYear = currentYear - 1;
+  updateDateObject();
+  $('.yearValue').text(currentYear)
+}
+
+upMonth = function(){
+  currentMonth = currentMonth + 1;
+  updateDateObject();
+  $('.monthValue').text(currentMonth)
+}
+
+downMonth = function(){
+  currentMonth = currentMonth - 1;
+  updateDateObject();
+  $('.monthValue').text(currentMonth)
+}
+
+upDay = function(){
+  currentDay = currentDay + 1;
+  updateDateObject();
+  $('.dayValue').text(currentDay)
+}
+
+downDay = function(){
+  currentDay = currentDay - 1;
+  updateDateObject();
+  $('.dayValue').text(currentDay)
+}
+
+// modify dates
+$('.upYear').click(upYear)
+$('.downYear').click(downYear)
+$('.upMonth').click(upMonth)
+$('.downMonth').click(downMonth)
+$('.upDay').click(upDay)
+$('.downDay').click(downDay)
+
+// set initial date values
+$('.yearValue').text(currentYear)
+$('.monthValue').text(currentMonth)
+$('.dayValue').text(currentDay)
+
+// update date functions END //
+
+function daysInMonth(month,year) { //Month is 1 based
+    return new Date(year, month, 0).getDate();
+}
 
 yearsToDate = _.range(1970, todayYear+1) // note: 1970 was the first javascript Date year
 
@@ -26,19 +98,17 @@ datesArray = _.range(1, daysThisMonth+1)
 dateObjects = _.map(datesArray, function(num){
                   return _.object(['date'], [num]); // return array of date hashes
                 })
+// date selector
+selectDate = function(){
+  var selector = '.datepicker_day';
 
-function ViewModel() {
-// note: knockout data-bindings require {key: pair} values
-    this.years = yearObjects;
-    this.chosenYear = ko.observable();
+  $(selector).on('click', function(){
+      $(selector).removeClass('active');
+      $(this).addClass('active');
+  });
+}
+// end date selector
 
-    this.months = monthsArray;
-    this.chosenMonth = ko.observable();
+logdate = function(d){console.log(d)}
 
-    this.dates = dateObjects;
-    this.chosenDate = ko.observable();
-
-  };
-
-  ko.applyBindings(new ViewModel());
 }); // end of $(document).ready
